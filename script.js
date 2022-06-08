611c8b816ee4775acb084b409e12c8df8396ace5
Vito <103535367+OCVitin@users.noreply.github.com>
2022-06-08 19:10:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+//Aqui temos o código em JavaScript. Nesse projeto, não vi muitas oportunidades de usar, mas aqui estão algumas funções que eu usei. Todas são funções para chamar o card ao clicar na imagem.
+
+function on1() {
+  document.getElementById("card1").style.display = "block";
+  document.getElementById("escuro").style.display = "block";}
+function off1() {
+  document.getElementById("card1").style.display = "none";
+  document.getElementById("escuro").style.display = "none";}
+
+function on2() {
+  document.getElementById("card2").style.display = "block";
+  document.getElementById("escuro").style.display = "block";}
+function off2() {
+  document.getElementById("card2").style.display = "none";
+  document.getElementById("escuro").style.display = "none";}
+
+function on3() {
+  document.getElementById("card3").style.display = "block";
+  document.getElementById("escuro").style.display = "block";}
+function off3() {
+  document.getElementById("card3").style.display = "none";
+  document.getElementById("escuro").style.display = "none";}
+
+function on4() {
+  document.getElementById("card4").style.display = "block";
+  document.getElementById("escuro").style.display = "block";}
+function off4() {
+  document.getElementById("card4").style.display = "none";
+  document.getElementById("escuro").style.display = "none";}
+
+function on5() {
+  document.getElementById("card5").style.display = "block";
+  document.getElementById("escuro").style.display = "block";}
+function off5() {
+  document.getElementById("card5").style.display = "none";
+  document.getElementById("escuro").style.display = "none";}
